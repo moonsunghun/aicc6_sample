0ef3323afd3e14f2f08ea70acc27aae247f0e6b9
moonsunghun <msh6618@naver.com>
2025-05-01 18:18:12 +0900

    adsfasdf

--- js/custom.js
+++ js/custom.js
@@ -6,18 +6,16 @@ const icon = document.querySelector(".menu i");
 menu.addEventListener("click", function(){
     this.classList.toggle("active");
 
-    // if(this.classList.contains("active"))
-    // {
-    //     // console.log("있다");
-    //     icon.setAttribute("class","ri-close-line");
-    // }
-    // else
-    // {
-    //     // console.log("없다");
-    //     icon.setAttribute("class","ri-menu-line");
-    // }
+    if(this.classList.contains("active"))
+    {
+        icon.setAttribute("class","ri-close-line");
+    }
+    else
+    {
+        icon.setAttribute("class","ri-menu-line");
+    }
 
-    this.classList.contains('active') ?  icon.setAttribute("class","ri-close-line") : icon.setAttribute("class","ri-menu-line") ;
+    //this.classList.contains('active') ?  icon.setAttribute("class","ri-close-line") : icon.setAttribute("class","ri-menu-line") ;
 
 
 })
