7758fbea59851e04e86267285f46d3eac254f30b
moonsunghun <msh6618@naver.com>
2025-05-02 12:18:12 +0900

    20250502_01

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,45 @@
 // menu toggle
 
-const menu = document.querySelector(".menu");
-const icon = document.querySelector(".menu i");
+const menu = document.querySelector('.menu');
+const icon = document.querySelector('.menu i');
+const nav = document.querySelector('.navi');
 
-menu.addEventListener("click", function(){
-    this.classList.toggle("active");
+menu.addEventListener('click', function () 
+{
+  this.classList.toggle('active');
 
-    if(this.classList.contains("active"))
-    {
-        icon.setAttribute("class","ri-close-line");
-    }
-    else
-    {
-        icon.setAttribute("class","ri-menu-line");
-    }
+  const navHeight = nav.scrollHeight;
 
-    //this.classList.contains('active') ?  icon.setAttribute("class","ri-close-line") : icon.setAttribute("class","ri-menu-line") ;
+  console.log(navHeight);
 
+  if (this.classList.contains('active')) 
+  {
+    icon.setAttribute('class', 'ri-close-line');
+    nav.style.height = navHeight + 'px';
+  } 
+  else 
+  {
+    icon.setAttribute('class', 'ri-menu-line');
+    nav.style.height = 0;
+  }
 
-})
+  //   this.classList.contains('active')
+  //     ? icon.setAttribute('class', 'ri-close-line')
+  //     : icon.setAttribute('class', 'ri-menu-line');
+});
 
-console.log(menu);
+// Navigation Activate
 
+const navs = document.querySelectorAll('.navi li');
 
+navs.forEach((nav, idx) => {
+  nav.addEventListener('click', function () {
+    navs.forEach((nav) => {
+      nav.classList.remove('active');
+    });
 
+    this.classList.add('active');
+  });
+});
 
-
-
+//console.log(navs);
